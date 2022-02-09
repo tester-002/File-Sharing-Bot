21df6da91f2f8c06302ce8dce8a1b0443ea4aea8
tester-002 <99108417+tester-002@users.noreply.github.com>
2022-02-09 18:20:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>coderzarena</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/tester-002/File-Sharing-Bot'>Click here</a>\n○ Group : @coderzarena\n○ Support Group : @coderzarenasupport</b>",	
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>coderzarena</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/tester-002/moviesgram'>Click here</a>\n○ Group : @coderzarena\n○ Support Group : @coderzarenasupport</b>",	
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
